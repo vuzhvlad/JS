b8e6b8369ea183e05d1304aab8fa0fc58ac3e1d5
vuzhvlad <vuzh.vlad@gmail.com>
2024-04-29 10:32:13 +0200

    starting to working with promises instead of callbacks

--- index.js
+++ index.js
@@ -6,13 +6,15 @@ fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
 
   superagent
     .get(`https://dog.ceo/api/breed/${data}/images/random/3`)
-    .end((err, res) => {
-      if (err) return console.log(err.message);
+    .then((res) => {
       console.log(res.body.message);
 
       fs.writeFile('dog-img.txt', JSON.stringify(res.body.message), (err) => {
         if (err) return console.log(err.message);
         console.log('Random dog image saved to file');
       });
-    }); // easy creating get request
+    })
+    .catch((err) => {
+      onsole.log(err.message);
+    });
 });

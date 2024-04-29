ff98dbe91c93d3f78ad164f2f23748ab18035403
vuzhvlad <vuzh.vlad@gmail.com>
2024-04-29 11:44:32 +0200

    adding promises, creating special prosified functions and doing chaining

--- index.js
+++ index.js
@@ -1,20 +1,42 @@
 const fs = require('fs');
 const superagent = require('superagent');
 
-fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
-  console.log(`Breed: ${data}`);
-
-  superagent
-    .get(`https://dog.ceo/api/breed/${data}/images/random/3`)
-    .then((res) => {
-      console.log(res.body.message);
-
-      fs.writeFile('dog-img.txt', JSON.stringify(res.body.message), (err) => {
-        if (err) return console.log(err.message);
-        console.log('Random dog image saved to file');
-      });
-    })
-    .catch((err) => {
-      onsole.log(err.message);
+const readFilePro = (file) => {
+  //passing file
+  return new Promise((resolve, reject) => {
+    // and returning promise
+    fs.readFile(file, (err, data) => {
+      // where we do all asyncchronous code
+      if (err) reject('I Could not find that file :('); // if not we give this error
+      resolve(data); // if everything is successful we pass the data
     });
-});
+  });
+};
+
+const writeFilePro = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, (err) => {
+      if (err) reject('Could not wire the file :(');
+      resolve('success');
+    });
+  });
+};
+
+readFilePro(`${__dirname}/dog.txt`)
+  .then((data) => {
+    console.log(`Breed: ${data}`);
+
+    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random/3`); // chaining promise to promise
+  })
+  .then((res) => {
+    console.log(res.body.message);
+    return writeFilePro('dog-img.txt', JSON.stringify(res.body.message));
+  })
+  .then(() => {
+    console.log('Random dog image saved to file');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+//chaining every promise to each promise by returning them in every return, so then they are connected
